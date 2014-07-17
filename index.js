509c8703b64785f4e48a746c2154d09b335547d2
DemetriousWilson <DemetriousWilson@gmail.com>
2014-07-16 22:24:37 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var logfmt = require('logfmt');
+
+var port = Number(process.env.PORT || 5000);
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.json({"message": "Hello World"});
+});
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
