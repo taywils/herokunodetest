c32bcfe5eb2334044f8a202fc1170f02eb38b7e4
DemetriousWilson <DemetriousWilson@gmail.com>
2014-07-17 15:42:59 -0700

    Updates for collectionsjs

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var app = require('express')();
 var logfmt = require('logfmt');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var paintDeque = require("collections/deque");
+require("collections/shim-array");
+require("collections/listen/array-changes");
 
 var port = Number(process.env.PORT || 5000);
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.json({"message": "Hello World", "testing": 123});
+  var testObj = {"message": "Hello World", "testing": 123};
+  res.json(testObj);
+  console.log(testObj);
 });
 
-app.listen(port, function() {
+http.listen(port, function() {
   console.log('Listening on ' + port);
 });
